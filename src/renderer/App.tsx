e61840fc64c454940a7d055aef63d9b3ab7ef6e8
edciriac <edciriac@jyu.fi>
2024-06-24 11:02:46 +0300

    Add file selection

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,27 @@
+/* eslint-disable no-console */
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { open } from 'fs/promises';
 
 function Main() {
+
+
+
   return (
     <div>
       <div className="content">
-
+        <div>
+          <p>test content</p>
+        </div>
+        <div className="file">
+          <input
+            type="file"
+            name="kube-config"
+            id="kube-config"
+            accept=".yaml,.yml"
+          />
+          <label htmlFor="kube-config"> test label </label>
+        </div>
       </div>
     </div>
   );
