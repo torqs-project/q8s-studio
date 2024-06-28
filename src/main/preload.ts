6abd12d4204e2a74dcc3cfa8104c6f89134fd43a
edciriac <edciriac@jyu.fi>
2024-06-28 10:01:06 +0300

    Fix type issue with electronAPI

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -2,8 +2,11 @@
 /* eslint no-unused-vars: off */
 import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('electronAPI', {
+const electronAPI = {
   openFile: (isDirectory: boolean) => ipcRenderer.invoke('openFile', isDirectory),
-  runCommand: (command: string) => {ipcRenderer.invoke('runCommand', command)},
-});
+  runCommand: (command: string) => ipcRenderer.invoke('runCommand', command),
+};
 
+contextBridge.exposeInMainWorld('electronAPI', electronAPI);
+
+export type ElectronAPI = typeof electronAPI;
